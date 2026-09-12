52d26e48e4ecbf7b74bb0832d0c112b861863575
CarlosPalmaSantos <carlospalmasantos4@gmail.com>
2026-09-12 12:17:41 +0200

    chore: adding dongo-shared

--- packages/dongo-shared/src/index.ts
+++ packages/dongo-shared/src/index.ts
@@ -1,5 +1,5 @@
 import { randomBytes } from "crypto";
-export import * as crypto from './crypto';
+export * as crypto from './crypto';
 
 export function RandomHex(size: number = 32, encoding: BufferEncoding = 'hex') {
   return randomBytes(size).toString(encoding);
